a5947ce8dc15b33ed5472ecbd9ea1bab749dec99
Tiz . <tirzah03pb@gmail.com>
2024-09-13 11:49:21 -0600

    Punto 2.4 código y evidencia

--- Practica2/Codigo/WebServer/WebServer.ino
+++ Practica2/Codigo/WebServer/WebServer.ino
@@ -1,9 +1,84 @@
-void setup() {
-  // put your setup code here, to run once:
+
+#include <WiFi.h>
+
+WiFiServer server(80);
+
+WiFiClient client;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+uint8_t temprature_sens_read();
+#ifdef __cplusplus
+}
+#endif
+
+uint8_t temprature_sens_read();
+
+void setup()
+{
+Serial.begin(9600);
+
+  WiFi.disconnect();
+  delay(3000);
+  Serial.println("Iniciando");
+  WiFi.begin("AGACENTRO","0192837465");
+  while ((!(WiFi.status() == WL_CONNECTED))){
+    Serial.print("......");
+    delay(300);
+
+  }
+  Serial.println("Conexion establecida con el SSID");
+  Serial.println((WiFi.localIP()));
+  server.begin();
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+
+void loop()
+{
+
+    client = server.available();
+    if (!client) { return; }
+    while(!client.available()){  delay(1); }
+    client.flush();
+    client.println("HTTP/1.1 200 OK");
+    client.println("Content-Type: text/html");
+    client.println("");
+    client.println("<!DOCTYPE HTML>");
+    client.println("<html>");
+    client.println("<head>");
+      client.println("<title> ""MiESP32"" </title>");
+    client.println("</head>");
+    client.println("<body>");
+      client.println("<h1 style=""color:#3333ff"">");
+      client.println("");
+      client.println("</h1>");
+      client.println("<span style=""color:#33ff33;font-size:14px"">");
+      client.println("Bienvenidos a mi pagina web");
+      client.println("</span>");
+      client.println("<br>");
+      client.println("<span style=""color:#ff6600;font-size:14px"">");
+      client.println("Esta pagina web ha estado activa por");
+      client.println("</span>");
+      client.println("<span style=""color:#009900;font-size:20px"">");
+      client.println((millis()/1000));
+      client.println("</span>");
+      client.println("<span style=""color:#ff6600;font-size:14px"">");
+      client.println("segundos");
+      client.println("</span>");
+      client.println("<br>");
+      client.println("<span style=""color:#ffcc66;font-size:14px"">");
+      client.println("La temperatura interna del ESP32 es de:");
+      client.println("</span>");
+      client.println("<span style=""color:#000000;font-size:20px"">");
+      client.println(((temprature_sens_read() - 32 ) / 1.8));
+      client.println("</span>");
+      client.println("<span style=""color:#ffcc66;font-size:14px"">");
+      client.println("grados");
+      client.println("</span>");
+    client.println("</body>");
+    client.println("</html>");
+    delay(1);
 
 }
